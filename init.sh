064473b7a3363b23d6a7c70fd6c4c3414c732b55
pedevineau <pedevineau@hotmail.fr>
2022-06-28 18:07:30 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,11 +8,12 @@ REPO_URL=https://github.com/etalab/matchSIRET.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
 # Put RFPE data in the working dir
-mc cp s3/projet-funathon/diffusion/rfpe.zip ${WORK_DIR}/rfpe.zip
+cd ${WORK_DIR}
+mc cp s3/projet-funathon/diffusion/rfpe.zip rfpe.zip
 unzip rfpe.zip
 
 # Install additional packages if needed
-REQUIREMENTS_FILE=${FORMATION_DIR}/requirements.txt
+REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE && rm $REQUIREMENTS_FILE
 
 # Remove course Git repository
