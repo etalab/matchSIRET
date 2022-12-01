545ff64e67b44c1611adb6bdcc58a804adee98f8
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-12-01 17:24:45 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,15 +4,14 @@ WORK_DIR=/home/onyxia/work
 CLONE_DIR=${WORK_DIR}/matchSIRET
 
 # Clone course repository
-REPO_URL=https://github.com/etalab/matchSIRET.git
+REPO_URL=git@github.com:etalab/matchSIRET.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
 # Put RFPE data in the working dir
 cd ${WORK_DIR}
 
-mc cp s3/projet-funathon/diffusion/rfpe.zip rfpe.zip # Avec l'user root
-
-unzip rfpe.zip
+#mc cp s3/projet-funathon/diffusion/rfpe.zip rfpe.zip # Avec l'user root
+#unzip rfpe.zip
 
 # Install additional packages if needed
 REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
