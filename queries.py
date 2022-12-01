f796a95e015db6bdf13f83f24e7131d0bafaafe8
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2022-12-01 19:02:28 +0100

    feat: update query and refacto notebook

--- queries.py
+++ queries.py
@@ -5,16 +5,15 @@ from worksites import GeoWorkSiteName
 
 
 def request_elastic(
-    conn: es.Elasticsearch, geowk: GeoWorkSiteName, geo_threshold: float = 0.7
+    conn: es.Elasticsearch, geowk: GeoWorkSiteName, geo_threshold: float = 0.7, use_geo: bool = True
 ) -> List[Dict]:
-    km_tolerance = 20
+    km_tolerance = 5
     q = {
         "bool": {
             "should": [
                 {
                     "multi_match": {
                         "query": geowk.__getattribute__("name"),
-                        "fuzziness" : "AUTO",
                         "fields": [
                             "nom_raison_sociale",
                             "nom_commercial",
@@ -26,74 +25,74 @@ def request_elastic(
             ]
         }
     }
+    if use_geo:
+        # TODO: the choice of thresholding on geocoding has strong impact. Adapt it to your usecase.
+        if geowk.score >= geo_threshold:
 
-    # TODO: the choice of thresholding on geocoding has strong impact. Adapt it to your usecase.
-    if geowk.score >= geo_threshold:
-        
-        q["bool"]["filter"] = {
-            "geo_distance": {
-                "distance": km_tolerance,
-                "coordonnees": {
-                    "lat": geowk.latitude,
-                    "lon": geowk.longitude
+            q["bool"]["filter"] = {
+                "geo_distance": {
+                    "distance": km_tolerance,
+                    "coordonnees": {
+                        "lat": geowk.latitude,
+                        "lon": geowk.longitude
+                    }
                 }
             }
-        }
-        
-        if geowk.__getattribute__("address"):
-            q["bool"]["should"].append(
-                {
-                    "multi_match": {
-                        "query": geowk.__getattribute__("address"),
-                        "fields": [
-                            "addresse_etablissement",
-                            "libelle_voie",
-                            "numero_voie",
-                        ],
-                    }
-                }
-            )
-        if geowk.__getattribute__("citycode"):
-            q["bool"]["should"].append(
-                {
-                    "match": {"commune": geowk.__getattribute__("citycode")}
-                }
-            )
-        if geowk.__getattribute__("cityname"):
-            q["bool"]["should"].append(
-                {
-                    "match": {"libelle_commune": geowk.__getattribute__("cityname")}
-                }
-            )
 
-    
-    else:
-        if geowk.__getattribute__("old_postcode"):
-            q["bool"]["should"].append(
-                {
-                    "match": {"code_postal": geowk.__getattribute__("old_postcode")}
-                }
-            )
-        if geowk.__getattribute__("old_cityname"):
-            q["bool"]["should"].append(
-                {
-                    "match": {"libelle_commune": geowk.__getattribute__("old_cityname")}
-                }
-            )
-
-        if geowk.__getattribute__("old_address"):
-            q["bool"]["should"].append(
-                {
-                    "multi_match": {
-                        "query": geowk.__getattribute__("old_address"),
-                        "fields": [
-                            "addresse_etablissement",
-                            "libelle_voie",
-                            "numero_voie",
-                        ],
+            if geowk.__getattribute__("address"):
+                q["bool"]["should"].append(
+                    {
+                        "multi_match": {
+                            "query": geowk.__getattribute__("address"),
+                            "fields": [
+                                "addresse_etablissement",
+                                "libelle_voie",
+                                "numero_voie",
+                            ],
+                        }
                     }
-                }
-            )
+                )
+            if geowk.__getattribute__("citycode"):
+                q["bool"]["should"].append(
+                    {
+                        "match": {"commune": geowk.__getattribute__("citycode")}
+                    }
+                )
+            if geowk.__getattribute__("cityname"):
+                q["bool"]["should"].append(
+                    {
+                        "match": {"libelle_commune": geowk.__getattribute__("cityname")}
+                    }
+                )
+
+
+        else:
+            if geowk.__getattribute__("old_postcode"):
+                q["bool"]["should"].append(
+                    {
+                        "match": {"code_postal": geowk.__getattribute__("old_postcode")}
+                    }
+                )
+            if geowk.__getattribute__("old_cityname"):
+                q["bool"]["should"].append(
+                    {
+                        "match": {"libelle_commune": geowk.__getattribute__("old_cityname")}
+                    }
+                )
+
+            if geowk.__getattribute__("old_address"):
+                q["bool"]["should"].append(
+                    {
+                        "multi_match": {
+                            "query": geowk.__getattribute__("old_address"),
+                            "fields": [
+                                "addresse_etablissement",
+                                "libelle_voie",
+                                "numero_voie",
+                            ],
+                        }
+                    }
+                )
 
 
     for field in list(geowk.__dict__):
